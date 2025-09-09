8079d952ad545a3526dcbd609a2648be4a3e8c23
Aditya Jadhav <159987858+AdityaJadhav17@users.noreply.github.com>
2025-09-08 19:26:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AdityaJadhav17.github.io/', // 👈 use your repo name
+  base: '/',
 })
